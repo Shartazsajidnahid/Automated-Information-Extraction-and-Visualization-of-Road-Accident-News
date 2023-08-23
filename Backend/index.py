906d47f047c5fb2bf1f9fc1adbcfce1da3af7f15
Shartazsajidnahid <sajidnahid9658@gmail.com>
2023-08-23 21:02:07 +0600

    news component

--- Backend/index.py
+++ Backend/index.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from api.routes import item, user
 
 app = FastAPI()
 
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Replace with your React app URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(item.router, prefix="/items", tags=["items"])
 app.include_router(user.router, prefix="/users", tags=["users"])
+
+
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,63 +1,23 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
-import "./App.css"; // Import your own custom CSS (if needed)
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Import Router and related components
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Allnews from "./News/Allnews";
+import Newsdetail from "./News/Newsdetail";
+// import Newsdetail from "./News/Newsdetail";
+
 
 function App() {
-  const [news, setNews] = useState([]);
-  const [newNews, setNewNews] = useState("");
-
-  const apiBaseUrl = process.env.REACT_APP_API_BASE_URL;
-
-  useEffect(() => {
-    axios.get(`${apiBaseUrl}/news/`).then((response) => {
-      setNews(response.data);
-    });
-  }, [apiBaseUrl]);
-
-  const handleAddNews = () => {
-    axios.post(`${apiBaseUrl}/news/`, { news: newNews }).then((response) => {
-      setNews([...news, newNews]);
-      setNewNews("");
-    });
-  };
-
-  const truncateText = (text, maxLength) => {
-    if (text.length > maxLength) {
-      return text.substring(0, maxLength) + "...";
-    }
-    return text;
-  };
-
   return (
-    <div className="container">
-      <h1 className="mt-4">News Articles</h1>
-      <div className="row">
-        {news.map((newsItem, index) => (
-          <div className="col-md-4 mb-4" key={index}>
-            <div className="card">
-              <div className="card-body">
-                <p className="card-text">{truncateText(newsItem.content, 100)}</p>
-                <a href={newsItem.link} >
-                  Read More
-                </a>
-              </div>
-            </div>
-          </div>
-        ))}
+    <Router>
+      <div className="container">
+        <Routes>
+          <Route path="/allnews" element={<Allnews />} />
+          <Route path="/" element={<Allnews />} />
+          <Route path="/news/:id" element={<Newsdetail />} />
+        </Routes>
       </div>
-      <div className="mt-4">
-        <input
-          type="text"
-          value={newNews}
-          onChange={(e) => setNewNews(e.target.value)}
-          className="form-control"
-        />
-        <button onClick={handleAddNews} className="btn btn-primary mt-2">
-          Add News
-        </button>
-      </div>
-    </div>
+    </Router>
   );
 }
 
