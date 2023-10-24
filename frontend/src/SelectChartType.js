63f0f19ad9cfc9ae6025b0e8388b90ed6b817f02
Shartazsajidnahid <sajidnahid9658@gmail.com>
2023-10-24 20:12:23 +0600

    SelectChartType ok, type and data select done in one file.

--- frontend/src/SelectChartType.js
+++ frontend/src/SelectChartType.js
@@ -1,75 +1,134 @@
-import React, { useState } from "react";
-import { Button, Form, Container, Row, Col } from "react-bootstrap";
-import { Link, useNavigate } from "react-router-dom";
+import React, { useState, useEffect, useRef } from "react";
+import { Form, Container, Row, Col } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import Chart from "chart.js/auto";
 
-function ChartGenerator() {
+function ChartPage() {
   const [chartType, setChartType] = useState("bar");
-  const [generateChart, setGenerateChart] = useState(false);
-  const navigate = useNavigate()
+  const [dataOption, setDataOption] = useState("vehicles");
+  const navigate = useNavigate();
+  const chartRef = useRef(null);
+  const chartDataRef = useRef(null);
+  const myChartRef = useRef(null);
 
-  const handleGenerateChart = () => {
-    setGenerateChart(true);
+  const vehicleData = [
+    { vehicle_type: "মোটরসাইকেল", count: 10 },
+    { vehicle_type: "বাইক", count: 4 },
+    { vehicle_type: "ভ্যান", count: 8 },
+    { vehicle_type: "গাড়ি", count: 20 },
+    { vehicle_type: "বাস", count: 5 }
+];
+
+
+  const placeData = [
+    { place: "ঢাকা", count: 12 },
+    { place: "বান্দরবান", count: 34 },
+    { place: "ময়মনসিংহ", count: 8 },
+    { place: "কক্সবাজার", count: 15 },
+    { place: "সিলেট", count: 22 }
+];
+
+  // Create a mapping of data options to their respective data and label
+  const dataOptions = {
+    vehicles: { data: vehicleData, label: "Vehicle Occurrence", key: "vehicle_type" },
+    places: { data: placeData, label: "Place Occurrences", key: "place" },
+  };
+
+  useEffect(() => {
+    if (chartRef.current) {
+      if (myChartRef.current) {
+        myChartRef.current.destroy();
+      }
+
+      myChartRef.current = new Chart(chartRef.current, {
+        type: chartType,
+        data: chartDataRef.current,
+        options: {
+          maintainAspectRatio: false,
+          responsive: false,
+        },
+      });
+
+      updateChart(chartType, dataOption);
+    }
+  }, [chartType, dataOption]);
+
+  const updateChart = (selectedChartType, selectedDataOption) => {
+    const selectedData = dataOptions[selectedDataOption].data;
+    const labelKey = dataOptions[selectedDataOption].key;
+
+    const labels = selectedData.map((item) => item[labelKey]);
+    const counts = selectedData.map((item) => item.count);
+
+    chartDataRef.current = {
+      labels: labels,
+      datasets: [
+        {
+          label: dataOptions[selectedDataOption].label,
+          data: counts,
+          borderWidth: 1,
+        },
+      ],
+    };
+
+    myChartRef.current.config.type = selectedChartType; // Update the chart type
+    myChartRef.current.update();
   };
 
   return (
-    <Container className="mt-5">
+    <Container className="mt-3">
       <Row className="justify-content-center">
         <Col md={6} className="bg-light p-4 rounded shadow">
-          <h1 className="text-center mb-4">Select Chart Type</h1>
+          <h2 className="text-center mb-4">Customize</h2>
           <Form>
-            <Form.Group controlId="chartType">
-              <Form.Check
-                type="radio"
-                label="Bar Chart"
-                id="bar"
-                checked={chartType === "bar"}
-                onChange={() => setChartType("bar")}
-              />
-              <Form.Check
-                type="radio"
-                label="Pie Chart"
-                id="pie"
-                checked={chartType === "pie"}
-                onChange={() => setChartType("pie")}
-              />
-              <Form.Check
-                type="radio"
-                label="Line Chart"
-                id="line"
-                checked={chartType === "line"}
-                onChange={() => setChartType("line")}
-              />
-              <Form.Check
-                type="radio"
-                label="Histogram"
-                id="histogram"
-                checked={chartType === "histogram"}
-                onChange={() => setChartType("histogram")}
-              />
-            </Form.Group>
-
-            <div className="text-center">
-              {/* <Link
-                to={{
-                  pathname: "/viewChart",
-                  state: { chartTypes: chartType },
-                }}
-              >
-              </Link> */}
-              <Button
-                variant="primary"
-                onClick={() => {
-                  navigate("/viewChart", {replace:true, state:{chartType}});
-                }}
-              >
-                Generate Chart
-              </Button>
-            </div>
+            <Row>
+              <Col md={6}>
+                <Form.Group controlId="chartType">
+                  <Form.Label><b>Chart Type</b></Form.Label>
+                  <Form.Control
+                    as="select"
+                    value={chartType}
+                    onChange={(e) => {
+                      const selectedChartType = e.target.value;
+                      setChartType(selectedChartType);
+                      updateChart(selectedChartType, dataOption);
+                    }}
+                  >
+                    <option value="bar">Bar Chart</option>
+                    <option value="pie">Pie Chart</option>
+                    <option value="line">Line Chart</option>
+                    <option value="radar">Radar Chart</option>
+                  </Form.Control>
+                </Form.Group>
+              </Col>
+              <Col md={6}>
+                <Form.Group controlId="dataOption">
+                  <Form.Label><b>Data Option</b></Form.Label>
+                  <Form.Control
+                    as="select"
+                    value={dataOption}
+                    onChange={(e) => {
+                      const selectedDataOption = e.target.value;
+                      setDataOption(selectedDataOption);
+                      updateChart(chartType, selectedDataOption);
+                    }}
+                  >
+                    <option value="vehicles">Vehicles</option>
+                    <option value="places">Places</option>
+                  </Form.Control>
+                </Form.Group>
+              </Col>
+            </Row>
           </Form>
+        </Col>
+      </Row>
+      <Row className="justify-content-center mt-4">
+        <Col md={8}>
+          <canvas ref={chartRef} width="1000" height="500"></canvas>
         </Col>
       </Row>
     </Container>
   );
 }
 
-export default ChartGenerator;
+export default ChartPage;
